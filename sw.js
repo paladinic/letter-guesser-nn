03bb4bf7aa5440a680ba291280b777b78cd59ff6
Claudio Paladini <clode.paladini@gmail.com>
2024-04-27 22:49:53 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,12 @@
+var GHPATH = '/letter-guesser-nn';
+ 
+var APP_PREFIX = 'lgnn_';
+ 
+var VERSION = 'version_00';
+ 
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/css/style.css`,
+  `${GHPATH}/js/script.js`
+]
